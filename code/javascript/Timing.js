074ebca03ea9ea4d9dba680c189e10ac129b0a34
TocaFonseca <49840274+TocaFonseca@users.noreply.github.com>
2020-05-24 12:33:22 +0100

    Few adjustments to the levels
    I added those times but we can change them easily

--- code/javascript/Timing.js
+++ code/javascript/Timing.js
@@ -1,14 +1,13 @@
 "use strict";
 
-export class Timing extends Phaser.Time.TimerEvent{    //not sure this is the phaser class that is extended?
+export class Timing extends Phaser.Time.TimerEvent{    
     
-    constructor(configs, level, scene) {
+    constructor(configs, level, scene, passing) {
         super(configs);
-        /*couldn't find the constructor for this phaser extension... once i do i'll replace it here*/
-
         this.level = level;
         this.scene = scene;
         this.count = 0;
+	this.countdown = passing;   //when we change screens preserves the time left
 	this.create();
     }
 	
@@ -18,51 +17,83 @@ export class Timing extends Phaser.Time.TimerEvent{    //not sure this is the ph
             fontFamily: "Comic Sans",
             color: "black"
         };	    
+	    
         if (this.level === 0){    //if level == 0 then the timer to view the tasks is the one initialized
-            this.countdown = 5;
+            this.countdown = 45; //45 seconds to view the tasks list
 	    console.log("Starting timer....");
             this.txt = this.scene.add.text(50,20,"Time Left: ", this.textConfigs);
             this.timer = this.scene.time.addEvent({
                 delay: 1000,
-                callback: this.update,
+                callback: this.updateTasks,
                 callbackScope: this,
                 repeat: this.countdown
             })
         } else {
-            /*
-                other levels
-            */
-        }
-    }
+            if (this.level === 1 && this.countdown === 0){
+                this.countdown = 5*60; //5 minutes 
 
-    /*
-        EXAMPLE: first level will be 5 minutes long and will simulate 12 hours of the day -> 25seg/hour
-    *//*
-    formatTime(){
-        this.count++;
-        if (this.level === 1){
-            var h = Math.floor(count/25000); 
-            var minutes = Math.floor(count%25000);
-        }
+            } else if (this.level === 2 && this.countdown === 0){ 
+                    this.countdown = 5*60; //5 minutes
         
-        //    other levels
-        
-        return (h+8) + ":" + minutes;	//example of day starting at 8 am
-    }*/
+            } else if (this.level === 3 && this.countdown === 0){
+                this.countdown = 4.5*60; //4.5 minutes
 
-    endLevel(){
-       console.log("Time's up!");
-    }
+            } else if (this.level === 4 && this.countdown === 0){
+                this.countdown = 4.5*60; //4.5 minutes
+
+            } else if (this.level === 5 && this.countdown === 0){
+                this.countdown = 4*60; //4 minutes
+
+            } else if (this.level === 6 && this.countdown === 0){
+                this.countdown = 4*60; //4 minutes
+
+            } else if (this.level === 7 && this.countdown === 0){
+                this.countdown = 3.5*60; //3.5 minutes
+            }
+
+            this.txt = this.scene.add.text(50,20,"Time: ", this.textConfigs);
+            this.timer = this.scene.time.addEvent({
+                delay: 416, //to simulate 25seg/h then every minute will be 416 ms
+                callback: this.updateLevel,
+                callbackScope: this,
+                repeat: this.countdown
+            });
+        }
 	
-    update(){
+	console.log("Starting timer....");
+    }
+
+    updateTasks(){
         if (this.count <= this.countdown){
-            //console.log(this.timer.getElapsed())
             this.txt.setText("Time Left: " + (this.countdown-this.count) +  " seconds");
             this.count++;
-            console.log("Its updating!!!");
         } else {
             this.endLevel();
         }
+    }
+	
+    updateLevel(){
+        if (this.count <= this.countdown){
+            this.txt.setText("Time: " + this.formatTime());
+            this.count++;
+        } else {
+            this.endLevel();
+        }
+    }
+
+    endLevel(){
+        console.log("Time's up!");
+        if (this.level === 0){
+            //go to next level
+        } else {
+            //end level
+        }
+    }
+	
+    formatTime(){    //25seg == 1 hour
+        var h = Math.floor(this.count/25000); 
+        var minutes = Math.floor(this.count%25000);
+        return (h+8) + ":" + minutes;	//day starting at 8 am
     }
 	
 	/*
